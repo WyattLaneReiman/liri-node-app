8e0365ef02ff8e5a06a40f676a2e3be5292ea8aa
Walt Disney <wyattreiman@Wyatts-MacBook-Pro.local>
2018-11-27 16:46:24 -0600

    added some stuff

---
+++ liri-node-app.js
@@ -0,0 +1,7 @@
+var axios = require("axios");
+
+axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(
+  function(response) {
+    console.log("The movie's rating is: " + response.data.imdbRating);
+  }
+);
